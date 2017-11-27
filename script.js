791a1a309fc60b87468ee32928bac9d16f38f5f8
Jeremy Ramirez <jramirez91@mail.stmarytx.edu>
2017-11-27 00:40:56 -0600

    home to index
    changed home to index

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.getElementById("foot01").innerHTML =
 
 document.getElementById("tabs ul").innerHTML =
 "<ul id='menu'>" +
-"<li><a href='home.html'>Home</a></li>" +
+"<li><a href='index.html'>Home</a></li>" +
 "<li><a href='members.html'>Members</a></li>" +
 "<li><a href='about.html'>About</a></li>" +
 "</ul>";
